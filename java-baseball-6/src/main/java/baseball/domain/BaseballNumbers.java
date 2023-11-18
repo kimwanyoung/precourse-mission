c56df0de1f3c8514c946eb7f2002b6cbcc6802b8
kimwanyoung <dhks2869@gmail.com>
2023-11-18 17:25:12 +0900

    feat : 점수 결과 반환 구현

--- java-baseball-6/src/main/java/baseball/domain/BaseballNumbers.java
+++ java-baseball-6/src/main/java/baseball/domain/BaseballNumbers.java
@@ -14,6 +14,35 @@ public class BaseballNumbers {
         this.baseballNumbers = baseballNumbers;
     }
 
+    public int calculateStrike(BaseballNumbers otherBaseballNumbers) {
+        int strike = 0;
+        for (int i = 0; i < baseballNumbers.size(); i++) {
+            int currentBaseballNumber = baseballNumbers.get(i);
+            int currentOtherBaseballNumber = otherBaseballNumbers.baseballNumbers.get(i);
+            if (currentBaseballNumber == currentOtherBaseballNumber) {
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+    public int calculateBall(BaseballNumbers otherBaseballNumbers) {
+        int ball = 0;
+        for (int i = 0; i < baseballNumbers.size(); i++) {
+            int currentBaseballNumber = baseballNumbers.get(i);
+            int currentOtherBaseballNumber = otherBaseballNumbers.baseballNumbers.get(i);
+            if (currentBaseballNumber != currentOtherBaseballNumber &&
+                    otherBaseballNumbers.contains(currentOtherBaseballNumber)) {
+                ball++;
+            }
+        }
+        return ball;
+    }
+
+    private boolean contains(Integer baseballNumber) {
+        return baseballNumbers.contains(baseballNumber);
+    }
+
     private void validateDuplicate(List<Integer> baseballNumbers) {
         Set<Integer> baseballNumberChecker = new HashSet<>(baseballNumbers);
         if (baseballNumberChecker.size() != 3) {
