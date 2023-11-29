7fecae3ca42af2fa98530c70e7c03316b59c6a5b
kimwanyoung <dhks2869@gmail.com>
2023-11-29 13:56:36 +0900

    feat : Controller 구현

--- java-racingcar-6/src/main/java/racingcar/Application.java
+++ java-racingcar-6/src/main/java/racingcar/Application.java
@@ -1,7 +1,28 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.service.RacingService;
+import racingcar.view.DefaultPrinter;
+import racingcar.view.DefaultReader;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RacingController racingController = new RacingController(
+                generateInputView(),
+                generateOutputView(),
+                new RacingService()
+        );
+
+        racingController.run();
+    }
+
+    public static InputView generateInputView() {
+        return new InputView(new DefaultPrinter(), new DefaultReader());
+    }
+
+    public static OutputView generateOutputView() {
+        return new OutputView(new DefaultPrinter());
     }
 }
---
+++ java-racingcar-6/src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,39 @@
+package racingcar.controller;
+
+import java.util.List;
+import racingcar.domain.RacingResultDto;
+import racingcar.service.RacingService;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RacingController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RacingService racingService;
+
+    public RacingController(
+            InputView inputView,
+            OutputView outputView,
+            RacingService racingService
+    ) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.racingService = racingService;
+    }
+
+    public void run() {
+        List<String> names = inputView.getCarNames();
+        int attempts = inputView.attempts();
+        racingService.generateCars(names);
+        raceMultipleTimes(attempts);
+    }
+
+    private void raceMultipleTimes(int attempts) {
+        for(int i = 0 ; i < attempts; i++) {
+            racingService.movingCars();
+            RacingResultDto racingResultDto = racingService.calculateRacingResult();
+            outputView.displayRacingResult(racingResultDto);
+        }
+    }
+}
